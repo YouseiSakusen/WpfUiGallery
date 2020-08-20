c2519cb72eac9a3852408c902d781c4b501b8c6c
Youseisakusen <reilo_okita@elf-mission.net>
2020-08-20 11:47:20 +0900

    MahApps.Metroを Ver.2.2.0 に更新。

---
+++ 04_MahAppsMDIX_dotNetCore/Module1/Module1Module.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Module1
+{
+	public class Module1Module : IModule
+	{
+		public void OnInitialized(IContainerProvider containerProvider)
+		{
+
+		}
+
+		public void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+
+		}
+	}
+}
---
+++ 04_MahAppsMDIX_dotNetCore/Module1/SamplePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Module1
+{
+	/// <summary>
+	/// Interaction logic for SamplePage
+	/// </summary>
+	public partial class SamplePage : UserControl
+	{
+		public SamplePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
